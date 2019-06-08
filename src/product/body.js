6d7ec41073f776a00b7168f96ddaee112eaa7e5a
ahakar <ahakar@gmail.com>
2019-06-08 11:35:47 -0400

    view cart implementation

--- src/product/body.js
+++ src/product/body.js
@@ -31,29 +31,32 @@ class body extends React.Component {
     render() {
     	return (
     		<div className="containerPage">
+					<div className="pt-3 text-right">
+						<button type="button" class="btn btn-outline-info">View Cart (0)</button>
+					</div>
     			<div className="row pt-3">
     				{(this.state.products.map((data, index) => {
-              			return(
-                			<div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 pb-3">
-	    					<div className="card border-dark h-100">
-	    						<div className="card-body">
-	    							<div className="row">
-		    							<div className="col-6 d-flex seperatorLine">
-		    								<img src={data.searchImage} alt="Login Form" className="border-0 img-thumbnail" />
-		    							</div>
-		    							<div className="col-6">
-		    								<p><strong> {data.product} </strong></p>
-		    								<p>price $ : {data.price}</p>
-		    								<p>Brand Name : <strong>{data.brand}</strong></p>
-		    								<p>Sizes Available : <footer> {data.sizes} </footer></p>
-		    								<button type="button" onClick={this.changeobj.bind(this, data)} className="btn btn-success btn-sm btn-block" data-toggle="modal" data-target="#exampleModal">Buy</button>
-		    							</div>
-	    							</div>
-	    						</div>
-	    					</div>
-	    				</div>
-              			);
-            		}))}
+							return(
+								<div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 pb-3">
+									<div className="card border-dark h-100">
+										<div className="card-body">
+											<div className="row">
+												<div className="col-6 d-flex seperatorLine">
+													<img src={data.searchImage} alt="Login Form" className="border-0 img-thumbnail" />
+												</div>
+												<div className="col-6">
+													<p><strong> {data.product} </strong></p>
+													<p>price $ : {data.price}</p>
+													<p>Brand Name : <strong>{data.brand}</strong></p>
+													<p>Sizes Available : <footer> {data.sizes} </footer></p>
+													<button type="button" onClick={this.changeobj.bind(this, data)} className="btn btn-success btn-sm btn-block" data-toggle="modal" data-target="#exampleModal">Buy</button>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							);
+						}))}
     			</div>
     			<Modal dataSend={this.state.product}/>
     		</div>
